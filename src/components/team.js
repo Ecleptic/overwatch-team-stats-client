c9341d0f333d4541b045a978e0750610e6f01c7e
Cameron Green <cgree78@gmail.com>
2018-08-08 23:05:21 -0500

    autocommit

--- src/components/team.js
+++ src/components/team.js
@@ -1,30 +1,34 @@
-import React from 'react'
+import React from "react"
 
-import Player from './player'
+import Player from "./player"
 
-export default function Team({players, isReversed, teamName}) {
-    return (
-        <div className='team'>
-            <h3 className='team__name'>{teamName}</h3>
+export default function Team({ players, isReversed, teamName }) {
+  return (
+    <div className="team">
+      <h3 className="team__name">{teamName}</h3>
 
-            <ul>
-                {players.map((player, index) => renderPlayer(player, index, isReversed))}
-            </ul>
-        </div>
-    )
+      <ul>
+        {players.map((player, index) =>
+          renderPlayer(player, index, isReversed)
+        )}
+      </ul>
+    </div>
+  )
 }
 
 function renderPlayer(player, index, isReversed) {
-    if (player.us) {
-        const iconUrl = player.us.stats.competitive.overall_stats.avatar
-        const rank = player.us.stats.competitive.overall_stats.comprank
-        const username = player.username
-        return <Player
-            key={index}
-            iconUrl={iconUrl}
-            isReversed={isReversed}
-            rank={rank}
-            username={username}/>
-    }
-
+  if (player.us) {
+    const iconUrl = player.us.stats.competitive.overall_stats.avatar
+    const rank = player.us.stats.competitive.overall_stats.comprank
+    const username = player.username
+    return (
+      <Player
+        key={index}
+        iconUrl={iconUrl}
+        isReversed={isReversed}
+        rank={rank}
+        username={username}
+      />
+    )
+  }
 }
